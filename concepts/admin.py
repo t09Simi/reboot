2b319ec4501e38498d795fef47a01cd4a43911c9
t09Simi <t09s22@abdn.ac.uk>
2026-03-04 12:19:20 +0530

    Concepts app created with jobs, skills and user assessments

---
+++ concepts/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Domain, JobRole, SkillCategory, Skill, UserSkillAssessment
+
+# Register your models here.
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order')
+
+@admin.register(JobRole)
+class JobRoleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'is_active')
+
+@admin.register(SkillCategory)
+class SkillCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name','category','job_role' )
+
+@admin.register(UserSkillAssessment)
+class UserSkillAssessmentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'skill')
---
+++ concepts/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.conf import settings
+
+class Domain(models.Model):
+    name = models.CharField(max_length=200)
+    icon = models.CharField(max_length=10, blank=True)  # emoji icon
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
+
+class JobRole(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+    domain = models.ForeignKey(
+    Domain,
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    related_name='job_roles')
+    created_by = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class SkillCategory(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    class SkillImportance(models.TextChoices):
+        REQUIRED = 'required', 'Required'
+        PREFERRED = 'preferred', 'Preferred'
+        BONUS = 'bonus', 'Bonus'
+
+    name = models.CharField(max_length=300, blank=True)
+    category = models.ForeignKey(SkillCategory, on_delete=models.CASCADE,related_name='skills')
+    job_role = models.ForeignKey(JobRole, on_delete=models.CASCADE, related_name='skills')
+    importance = models.CharField(
+        max_length=20,
+        choices=SkillImportance.choices,
+        default=SkillImportance.REQUIRED
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+    
+class UserSkillAssessment(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='skill_assessments')
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='assessments')
+    knows_skill = models.BooleanField(default=True)
+    assessed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'skill')
+
+    def __str__(self):
+        return f"{self.user.name} - {self.skill.name}"
